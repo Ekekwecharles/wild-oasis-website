b59a2148b0be98f1c5ba5c736ab3a28bd41509da
Charlessnow <ekekwecharles50@gmail.com>
2025-01-18 02:30:28 +0100

    feat(Sidebar): add an uploader button, so i can easily reload fresh data
    Changed the supabase database and added a file containing the login details for first time users (Applicaation Password)

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://csvsenjjwidwiviyykga.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzdnNlbmpqd2lkd2l2aXl5a2dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNjU5MDgsImV4cCI6MjA1MTk0MTkwOH0.uYo5rNvEprKVbUc4YfB13tXfjecJM2vtng3j3h4mFZw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
